03764b2b1cd60c86aac3353be940b33b0790bc6e
Anas ALhussain <64418002+AnasAlhussain@users.noreply.github.com>
2025-06-25 17:48:45 +0200

    first commit

---
+++ public/data/about.ts
@@ -0,0 +1,34 @@
+export const aboutMe = {
+  description: [
+    "Full Stack .NET Developer & Educator with several years of industry experience in backend, frontend, and database development. Currently working in Sweden as a consultant and instructor, teaching system development with a focus on C#, .NET, REST APIs, system architecture, and modern frontend frameworks like React, Angular, Blazor, and MVC. Passionate about clean code, automated testing (TDD/BDD), and mentoring the next generation of developers..",
+    "Experienced in leadership, teaching, and mentoring. Skilled in front-end and back-end development, and can deliver results under pressure. Passionate about building scalable web applications and contributing to innovative projects.",
+  ],
+  // Simply upload your CV to your Google Drive, then just copy the link to the uploaded file and paste it here.
+  cvDriveLink:
+    "https://drive.google.com/file/d/1vqkYvwaAUv3WlqJh1cg4PuwS3PFXBjSM/view?usp=drive_link",
+};
+
+export const technicalSkills: Record<string, string[]> = {
+  // You can rename the type of skills, it's dynamically rendered. eg: You can change "Programming Languages" -> to just "Languages"
+  "Programming Languages": ["C#", "JavaScript", "TypeScript", "Python"],
+  "Frontend Development": [
+    "Blazor",
+    "ASP.NET MVC",
+    "React",
+    "Angular",
+    "HTML5",
+    "CSS3",
+    "Bootstrap",
+    "Tailwind",
+  ],
+  "Backend Development": [".NET", "MongoDB", "SQL"],
+  "UI/UX Design": ["Figma", "Adobe Illustrator"],
+  "Tools & Technologies": ["Git", "GitHub", "Agile (Scrum)", "CI/CD"],
+  "Soft Skills": [
+    "Presentation",
+    "Communication",
+    "Professional Demeanor",
+    "Leadership",
+    "Mentorship",
+  ],
+};
